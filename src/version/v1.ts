d54b35c5c0972a1a9f27374892359e6cec339a63
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-12-24 16:44:46 +0700

    feat: add package swagger-jsdoc

--- src/version/v1.ts
+++ src/version/v1.ts
@@ -3,7 +3,9 @@ import AuthRouter from "../v1/routers/auth-router";
 import ProfileRouter from "../v1/routers/profile-router";
 import ReviewRouter from "../v1/routers/review-router";
 import swaggerUI from "swagger-ui-express";
-import swaggerDocument from "../../swagger/swagger-output.json";
+// import swaggerDocument from "../../swagger/swagger-output.json";
+import swaggerJsDoc from "swagger-jsdoc";
+
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
@@ -12,10 +14,35 @@ const V1Router = Router();
 V1Router.use(AuthRouter);
 V1Router.use(ProfileRouter);
 V1Router.use(ReviewRouter);
-V1Router.use(
-  "/",
-  swaggerUI.serve,
-  swaggerUI.setup(swaggerDocument, { customCssUrl: CSS_URL })
-);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
+    },
+    servers: [
+      {
+        url: "https://nodejs-swagger-api.vercel.app/",
+        description: "My API Documentation",
+      },
+    ],
+  },
+  // This is to call all the file
+  apis: ["src/**/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+V1Router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL }));
 
 export default V1Router;
