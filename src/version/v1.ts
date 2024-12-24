20e1fb7b10011e830621a091fd3009d79063c683
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-12-24 18:12:12 +0700

    feat: test deployment api

--- src/version/v1.ts
+++ src/version/v1.ts
@@ -3,8 +3,8 @@ import AuthRouter from "../v1/routers/auth-router";
 import ProfileRouter from "../v1/routers/profile-router";
 import ReviewRouter from "../v1/routers/review-router";
 import swaggerUI from "swagger-ui-express";
-// import swaggerDocument from "../../swagger/swagger-output.json";
-import swaggerJsDoc from "swagger-jsdoc";
+import swaggerDocument from "../../swagger/swagger-output.json";
+// import swaggerJsDoc from "swagger-jsdoc";
 import  bodyParser  from 'body-parser';
 
 const CSS_URL =
@@ -27,33 +27,33 @@ V1Router.use(ReviewRouter);
 V1Router.use(bodyParser.json()); // to use body object in requests
 
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-      termsOfService: "http://example.com/terms/",
-      contact: {
-        name: "API Support",
-        url: "http://www.exmaple.com/support",
-        email: "support@example.com",
-      },
-    },
-    servers: [
-      {
-        url: "https://nodejs-swagger-api.vercel.app/",
-        description: "My API Documentation",
-      },
-    ],
-  },
-  // This is to call all the file
-  apis: ["src/**/*.js"],
-};
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Library API",
+//       version: "1.0.0",
+//       description: "A simple Express Library API",
+//       termsOfService: "http://example.com/terms/",
+//       contact: {
+//         name: "API Support",
+//         url: "http://www.exmaple.com/support",
+//         email: "support@example.com",
+//       },
+//     },
+//     servers: [
+//       {
+//         url: "https://nodejs-swagger-api.vercel.app/",
+//         description: "My API Documentation",
+//       },
+//     ],
+//   },
+//   // This is to call all the file
+//   apis: ["src/**/*.js"],
+// };
 
-const specs = swaggerJsDoc(options);
+// const specs = swaggerJsDoc(options);
 
-V1Router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL }));
+V1Router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, { customCssUrl: CSS_URL }));
 
 export default V1Router;
